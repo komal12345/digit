b83b486c7c1d0dd493101c631ba93c6b4a9deca2
komal <komalshahzadi1992@gmail.com>
2016-05-28 15:50:20 +0500

    added wonderful files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'post_list.html', {'posts':posts})
